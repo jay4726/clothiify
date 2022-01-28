a0d5cab765002fbba5bbf4369304bdcbc563431b
jay4726 <76470342+jay4726@users.noreply.github.com>
2022-01-28 10:27:25 +0530

    Add files via upload

---
+++ Clothifyhub/Home/search/search.js
@@ -0,0 +1,50 @@
+// getting all required elements
+const searchWrapper = document.querySelector(".hitem");
+const inputBox = document.querySelector("input");
+const suggBox = document.querySelector(".autocom-box");
+const icon = document.querySelector(".icon");
+let linkTag = document.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toString().toLocaleLowerCase().startsWith(userData.toString().toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+ 
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
---
+++ Clothifyhub/Home/search/suggestions.js
@@ -0,0 +1,15 @@
+let suggestions = [
+    ["tshirts for men"],
+    ["jeans for men"],
+    ["shoes for men"],
+    ["suits for men"],
+    ["caps for men"],
+    ["tops for women"],
+    ["ethnic wear for women"],
+    ["flats for women" ],
+    ["jeans for women" ],
+    ["kurta for Women"],
+    ["tshirts for kids"],
+    ["jeans for kids"],
+    ["shoes for kids"]
+]
